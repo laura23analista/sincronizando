fb481da17228dbd887215321f34511ace33733a4
Laura <laurita23analista@gmail.com>
2020-07-29 11:53:40 -0300

    se agrego la estructura básica de HTML

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="e">
+<head>
+
+   <meta charset="UTF-8">
+  <meta name="viewport" content="width=divice-width",
+initial-scaler1.0>
+ <meta http-equiv="X-UA-Compatible"
+content="ie=edge">
+ <title>Document</title>
+
+</head>
+</body>
+
+</body>
+</html>
